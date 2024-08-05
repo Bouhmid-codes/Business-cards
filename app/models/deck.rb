b9ecace30ad5808c3317023a509764f6fc5d6b0f
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-08-05 09:03:45 +0200

    Updated Deck model to dependently destroy BusinessesDeck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,5 @@
 class Deck < ApplicationRecord
   belongs_to :user
-  has_many :businesses_decks
+  has_many :businesses_decks, dependent: :destroy
   has_many :businesses, through: :businesses_decks
 end
